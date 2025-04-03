5e060c511c15c9d936b6204185ae0d3d9131cc27
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-04-03 07:42:06 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let inputAdults = document.getElementById("adults")
 let inputKids = document.getElementById("kids")
-let inputDuration = document.getElementById("duration");
+let inputDuration = document.getElementById("duration")
 
 let result = document.getElementById("result");
 
